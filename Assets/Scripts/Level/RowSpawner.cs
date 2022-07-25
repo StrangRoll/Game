dda5754284a59424c2aa91f72f4baa7a817ba505
StrangRoll <93737752+StrangRoll@users.noreply.github.com>
2022-07-25 22:06:16 +0300

    Added row pool

--- Assets/Scripts/Level/RowSpawner.cs
+++ Assets/Scripts/Level/RowSpawner.cs
@@ -17,14 +17,24 @@ public class RowSpawner : MonoBehaviour
     private float _rightSpawnX;
     private float _spawnHeight;
     private IEnumerator _spawnCoroutine;
+    private Row[] _rows;
+    private int _rowCount = 5;
 
-    private void Start()
+    private void Awake()
     {
         _leftSpawnX = _leftSpawnPosition.position.x;
         _rightSpawnX = _rightSpawnPosition.position.x;
         var screenHeight = _camera.ViewportToScreenPoint(Vector3.up).y;
         var rowHeight = _rowPrefab.GetComponent<SpriteRenderer>().sprite.rect.height;
         _spawnHeight = screenHeight + rowHeight / 2;
+        _rows = new Row[_rowCount];
+
+        for (int i = 0; i < _rowCount; i++)
+        {
+            var newRow = Instantiate(_rowPrefab, Vector3.zero, Quaternion.identity, transform);
+            newRow.gameObject.SetActive(false);
+            _rows[i] = newRow;
+        }
     }
 
     private void OnEnable()
@@ -40,22 +50,37 @@ public class RowSpawner : MonoBehaviour
 
     private IEnumerator RowSpawnTimer()
     {
+        for (int i = 0; i < _rowCount; i++)
+        {
+            _rows[i].gameObject.SetActive(false);
+        }
+
+        var rowIndex = 0;
+
         while (true)
         {
             var timeBeforNewRow = Random.Range(_minSecondsBetweenSpawn, _maxSecondsBetweenSpawn);
-            yield return new WaitForSeconds(timeBeforNewRow);
             var newRowX = Random.Range(_leftSpawnX, _rightSpawnX);
             var newRowY = _camera.ScreenToWorldPoint(Vector3.up * _spawnHeight).y;
             var newRowPosition = new Vector3(newRowX, newRowY, 0);
-            var newRow = Instantiate(_rowPrefab, newRowPosition, Quaternion.identity, transform);
+            var newRow = _rows[rowIndex];
+            newRow.gameObject.SetActive(true);
+            newRow.transform.position = new Vector3(newRowX, newRowY, 0);
             var newRowSpeed = Random.Range(_minRowSpeed, _maxRowSpeed);
             int newRowDirection;
+
             if (Random.Range(0, 2) == 0)
                 newRowDirection = RowDirection.Right;
             else
                 newRowDirection = RowDirection.Left;
 
             newRow.Init(newRowDirection, newRowSpeed);
+            rowIndex++;
+
+            if (rowIndex >= _rowCount)
+                rowIndex = 0;
+
+            yield return new WaitForSeconds(timeBeforNewRow);
         }
     }
 }
